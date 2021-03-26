ecfabb2a10926fdbec21eea3796cb4aeeea4d040
PowerBall253 <brunoanconasala@gmail.com>
2021-03-26 08:27:51 -0600

    Upload source

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # idCryptRust
 Command line utility to encrypt/decrypt DOOM's .bfile and .blang files.
+
+## Usage
+```
+idCrypt  <file-path> <internal-file-path>
+```
+The tool will try to decrypt if the file ends in .blang or .bfile, and encrypt otherwise.
+
+## Compiling
+### Linux / macOS
+To compile, you'll need a Rust environment set up with rustup. You can set it up by running:
+```
+curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
+```
+and following the given instructions.
+
+Afterwards, clone this repo:
+```
+git clone https://github.com/PowerBall253/idCryptRust.git
+```
+
+Finally, cd into the directory and compile with cargo:
+```
+cd idCryptRust
+cargo build --release
+```
+The compiled binary will be located at the ./target/release folder.
+
+### Windows
+To compile, you'll need a Rust environment set up with rustup and the Visual Studio C++ build tools. You can set it up by downloading rustup from [here](https://www.rust-lang.org/tools/install) and follow the given instructions, then downloading Visual Studio 2019 and selecting the C++ tools for download.
+
+NOTE: All the following commands are for PowerShell.
+
+Afterwards, clone this repo using the Git Bash:
+```
+git clone https://github.com/PowerBall253/idCryptRust.git
+```
+
+Finally, cd into the directory and compile with cargo:
+```
+cd idCryptRust
+cargo build --release
+```
+The compiled binary will be located at the .\target\release folder.
+
+## Credits
+* emoose: Creator of the original idCrypt tool.
---
+++ src/id_crypt.rs
@@ -0,0 +1,139 @@
+#![allow(non_snake_case)]
+
+extern crate getrandom;
+extern crate sha2;
+extern crate hmac;
+extern crate aes;
+extern crate block_modes;
+
+use std::path::Path;
+use std::ffi::OsStr;
+use std::fs::File;
+use std::io::prelude::*;
+use getrandom::getrandom;
+use sha2::{Sha256, Digest};
+use hmac::{Hmac, Mac, NewMac};
+use aes::Aes128;
+use block_modes::{BlockMode, Cbc};
+use block_modes::block_padding::Pkcs7;
+
+type HmacSha256 = Hmac<Sha256>;
+type Aes128Cbc = Cbc<Aes128, Pkcs7>;
+
+fn main() {
+    println!("id_crypt v1.0 by PowerBall253\n");
+
+    let key_derive_static = "swapTeam\n".to_string();
+
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() < 3 {
+		println!("Usage:\n\tidCrypt.exe <file-path> <internal-file-path>\n");
+		println!("Example:\n\tidCrypt.exe D:\\english.bfile strings/english.lang\n");
+		println!("If a .bfile is supplied it'll be decrypted to <file-path>.dec");
+		println!("Otherwise the file will be encrypted to <file-path>.bfile\n");
+		println!("You _must_ use the correct internal filepath for decryption to succeed!");
+        std::process::exit(1);
+    }
+
+    let mut decrypt = false;
+
+    let file_path = &args[1];
+    let internal_path = &args[2];
+    let extension = String::from(Path::new(file_path).extension().and_then(OsStr::to_str).unwrap());
+
+    if extension.to_lowercase() == "blang" || extension.to_lowercase() == "bfile" {
+        decrypt = true;
+    }
+
+    let dest_path: String;
+
+    if decrypt {
+        dest_path = format!("{}.{}", file_path, "dec");
+    }
+    else {
+        dest_path = format!("{}.{}", file_path, "blang");
+    }
+
+    let mut file = File::open(file_path).expect("Failed to open file for reading."); 
+    let mut file_data: Vec<u8> = Vec::new();
+    file.read_to_end(&mut file_data).expect("Failed to read file.");
+    let size = file_data.len();
+
+    let mut file_salt: Vec<u8>;
+
+    if decrypt {
+        file_salt = file_data[0..0xC].to_vec();
+    }
+    else {
+        file_salt = vec![0u8; 0xC];
+        getrandom(&mut file_salt).unwrap();
+    }
+
+    let mut hasher = Sha256::new();
+    hasher.update(&file_salt);
+    let mut empty_byte_array = vec![0; 1];
+    let mut key_derive_static_bytes = key_derive_static.as_bytes().to_vec();
+    key_derive_static_bytes.append(&mut empty_byte_array);
+    hasher.update(&key_derive_static_bytes);
+    hasher.update(&internal_path);
+    let enc_key = hasher.finalize().to_vec();
+    
+    let mut file_iv = vec![0u8; 0x10];
+
+    if decrypt {
+        file_iv = file_data[0xC..(0xC + 0x10)].to_vec();
+    }
+    else {
+        getrandom(&mut file_iv).unwrap();
+    }
+
+    let mut file_text = file_data.clone();
+    let hmac: Vec<u8>;
+
+    if decrypt {
+        file_text = file_data[0x1C..(size as usize - 0x20)].to_vec();
+
+        let file_hmac = file_data[(size as usize - 0x20)..(size as usize)].to_vec();
+
+        let mut mac_hasher = HmacSha256::new_varkey(&enc_key).unwrap();
+        mac_hasher.update(&file_salt);
+        mac_hasher.update(&file_iv);
+        mac_hasher.update(&file_text);
+        hmac = mac_hasher.finalize().into_bytes().to_vec();
+
+        assert_eq!(hmac.to_vec(), file_hmac);
+    }
+
+    let cipher = Aes128Cbc::new_var(&enc_key[0..0x10].to_vec(), &file_iv).unwrap();
+    let crypted_text: Vec<u8>;
+
+    if decrypt {
+        crypted_text = cipher.decrypt_vec(&file_text).unwrap();
+    }
+    else {
+        crypted_text = cipher.encrypt_vec(&file_text);
+    }
+
+    let mut file = File::create(&dest_path).expect("Failed to open destination path for writing.");
+
+    if decrypt {
+        file.write_all(&crypted_text).expect("Failed to write new contents to destination path");
+
+        println!("Decryption succeeded! Wrote to {}\n", dest_path);
+    }
+    else {
+        file.write(&file_salt).expect("Failed to write new contents to destination path.");
+        file.write(&file_iv).expect("Failed to write new contents to destination path.");
+        file.write(&crypted_text).expect("Failed to write new contents to destination path.");
+
+        let mut mac_hasher = HmacSha256::new_varkey(&enc_key).unwrap();
+        mac_hasher.update(&file_salt);
+        mac_hasher.update(&file_iv);
+        mac_hasher.update(&crypted_text);
+        let hmac = mac_hasher.finalize().into_bytes().to_vec();
+        file.write(&hmac).expect("Failed to write new contents to destination path.");
+
+        println!("Encryption succeeded! Wrote to {}\n", dest_path);
+    }
+}
